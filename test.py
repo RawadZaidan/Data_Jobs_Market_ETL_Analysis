55ea0d3ff6205020e0bf48ffd5d6f2cb2038cfcd
RawadZaidan <rawad.o.zaidan@gmail.com>
2023-10-08 11:35:54 +0300

    Linkedin initial function done

--- test.py
+++
@@ -1,9 +0,0 @@
-from main import job_data_get
-from requests_html import HTMLSession
-
-s = HTMLSession()
-u = 'https://www.linkedin.com/jobs/search?keywords=data%20engineer&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
-resp = s.get(u)
-prod = resp.html.xpath('//ul[@class = "jobs-search__results-list"]')
-for p in prod:
-    print(p.text)
